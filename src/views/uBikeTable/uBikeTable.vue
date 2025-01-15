bfd9c13ab1fa2fe1b5a25c1388441888bb3d1dc7
tingfer <tingfer@gmail.com>
2025-01-15 08:42:27 +0800

    pagination.vue

--- src/views/uBikeTable/uBikeTable.vue
+++ src/views/uBikeTable/uBikeTable.vue
@@ -2,6 +2,7 @@
 import { ref, computed, watch } from 'vue';
 import search from './components/search.vue';
 import uBikeTable from './components/uBikeTable.vue';
+import pagination from './components/pagination.vue';
 
 // 修改這份 YouBike 即時資訊表，並加上
 // 1. 站點名稱搜尋
@@ -141,30 +142,13 @@ const setSort = sortType => {
     />
   </div>
 
-  <!-- 頁籤 -->
-  <nav v-if="pagerEnd > 0">
-    <ul class="pagination">
-
-      <li @click.prevent="setPage(1)" class="page-item">
-        <a class="page-link" href>第一頁</a>
-      </li>
-      <li @click.prevent="setPage(currentPage - 1)" class="page-item">
-        <a class="page-link" href>&lt;</a>
-      </li>
-
-      <li v-for="i in pagerEnd" :class="{ active: i + pagerAddAmount === currentPage }" :key="i"
-        @click.prevent="setPage(i + pagerAddAmount)" class="page-item">
-        <a class="page-link" href>{{ i + pagerAddAmount }}</a>
-      </li>
-
-      <li @click.prevent="setPage(currentPage + 1)" class="page-item">
-        <a class="page-link" href>&gt;</a>
-      </li>
-      <li @click.prevent="setPage(totalPageCount)" class="page-item">
-        <a class="page-link" href>最末頁</a>
-      </li>
-    </ul>
-  </nav>
+  <pagination
+      :pagerEnd="pagerEnd"
+      :currentPage="currentPage"
+      :totalPageCount="totalPageCount"
+      :pagerAddAmount="pagerAddAmount"
+      :setPage="setPage"
+    />
 </template>
 
 <style lang="scss" scoped>
